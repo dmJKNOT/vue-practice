606844ac9fe037443af86350043dcfc752b18222
unknown <jmwalters.guitar@gmail.com>
2023-09-12 19:57:49 -0400

    button toggles the content on and off

---
+++ src/components/ApplicationContainer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex" id="wrapper">
+    <ContentRaterSidebar/>
+    <div id="page-content-wrapper">
+      <ContentRaterNavbar @toggle-content="toggleContent"/>
+      <ContentRaterPageContent v-if="display"/>
+    </div>
+  </div>
+</template>
+<script>
+import ContentRaterSidebar from "@/components/ContentRaterSidebar";
+import ContentRaterPageContent from "@/components/ContentRaterPageContent";
+import ContentRaterNavbar from "@/components/ContentRaterNavbar";
+export default {
+  components: {ContentRaterNavbar, ContentRaterPageContent, ContentRaterSidebar},
+  data: () => ({ display: true }),
+  methods: {
+    toggleContent() {
+      this.display = !this.display;
+    }
+  },
+  emits:['toggle-content']
+}
+</script>
+
+<style src="../assets/css/styles.css"/>
---
+++ src/components/ContentRaterSidebar.vue
@@ -0,0 +1,20 @@
+<template >
+  <!-- Sidebar-->
+  <div class="border-end bg-white" id="sidebar-wrapper">
+    <div class="sidebar-heading border-bottom bg-light">Content Rater</div>
+    <div class="list-group list-group-flush">
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="movies">Movies</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="tv">TV</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="videoGames">Video Games</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="podcasts">Podcasts</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="books">Books</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentRaterSidebar"
+}
+</script>
+
